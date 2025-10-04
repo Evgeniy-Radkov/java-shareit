8f7409ccb13979b7cf715aee1b8865032e43b052
Evgeniy Radkov <boss.radkov2000@yandex.ru>
2025-10-04 13:03:40 +0300

    Fix:checkstyle

--- src/main/java/ru/practicum/shareit/constant/Headers.java
+++ src/main/java/ru/practicum/shareit/constant/Headers.java
@@ -1,6 +1,9 @@
 package ru.practicum.shareit.constant;
 
 public final class Headers {
-    private Headers() {}
+
+    private Headers() {
+    }
+
     public static final String X_SHARER_USER_ID = "X-Sharer-User-Id";
 }
